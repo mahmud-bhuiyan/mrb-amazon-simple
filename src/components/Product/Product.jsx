b96d05e417ab0e5c662aba5a8229a2ca496d5add
mahmud-bhuiyan <bh.mahmud@gmail.com>
2023-03-28 20:31:02 +0600

    product displayed

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Product.css";
+
+const Product = (props) => {
+  const { id, img, name, price, quantity, ratings, seller } = props.product;
+  return (
+    <div className="product">
+      <img src={img} alt="" />
+      <div className="product-info">
+        <h6 className="product-name">{name}</h6>
+        <p>Price: ${price}</p>
+        <p>Manufacturer: {seller}</p>
+        <p>Rating: {ratings} star</p>
+      </div>
+      <button className="btn-cart" onClick={() => {}}>
+        Add To Cart
+      </button>
+    </div>
+  );
+};
+
+export default Product;
